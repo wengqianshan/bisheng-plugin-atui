1c59828668edd9aa0180c155f8e7efa9b3c06017
弱冠 <qianshan.wengqs@alibaba-inc.com>
2016-07-15 18:31:20 +0800

    from bisheng-plugin-antd

---
+++ src/browser.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router';
+import toReactComponent from 'jsonml-to-react-component';
+import JsonML from 'jsonml.js/lib/utils';
+import VideoPlayer from './VideoPlayer';
+import ImagePreview from './ImagePreview';
+
+function isHeading(node) {
+  return /h[1-6]/i.test(JsonML.getTagName(node));
+}
+
+// export default doesn't work
+module.exports = () => {
+  return {
+    converters: [
+      [(node) => JsonML.isElement(node) && isHeading(node), (node, index) => {
+        const children = JsonML.getChildren(node);
+        return React.createElement(JsonML.getTagName(node), {
+          key: index,
+          id: children,
+          ...JsonML.getAttributes(node),
+        }, [
+          <span key="title">{children.map((child) => toReactComponent(child))}</span>,
+          <a href={`#${children}`} className="anchor" key="anchor">#</a>,
+        ]);
+      }],
+      [(node) => JsonML.isElement(node) && JsonML.getTagName(node) === 'video', (node, index) =>
+        <VideoPlayer video={JsonML.getAttributes(node)} key={index} />,
+      ],
+      [(node) => JsonML.isElement(node) && JsonML.getTagName(node) === 'a' && !(
+        JsonML.getAttributes(node).class ||
+          (JsonML.getAttributes(node).href &&
+           JsonML.getAttributes(node).href.indexOf('http') === 0) ||
+          /^#/.test(JsonML.getAttributes(node).href)
+      ), (node, index) => {
+        return <Link to={JsonML.getAttributes(node).href} key={index}>{toReactComponent(JsonML.getChildren(node)[0])}</Link>;
+      }],
+      [(node) => {
+        return JsonML.isElement(node) &&
+          JsonML.getTagName(node) === 'p' &&
+          JsonML.getTagName(JsonML.getChildren(node)[0]) === 'img' &&
+          /preview-img/gi.test(JsonML.getAttributes(JsonML.getChildren(node)[0]).class);
+      }, (node, index) => {
+        const imgs = JsonML.getChildren(node)
+                .filter((img) => JsonML.isElement(img) && Object.keys(JsonML.getAttributes(img)).length > 0)
+                .map((img) => JsonML.getAttributes(img));
+        return <ImagePreview imgs={imgs} key={index} />;
+      }],
+    ],
+  };
+};
